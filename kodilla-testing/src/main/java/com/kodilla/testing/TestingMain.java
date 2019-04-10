6f04afa059375df1964235835aa7c645358b067b
SzymonGrzybowicz <szymon.grzybowicz@gmail.com>
2019-04-10 12:49:47 +0200

    Wykonanie zadania 6.2, test klasy calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+
+        System.out.println("Test metody add:");
+        if(Calculator.add(2 ,3 ) == 5){
+            System.out.println("Test zakończony poprawnie!");
+        }
+        else{
+            System.out.println("Test zakończony negatywnie!");
+        }
+
+
+        System.out.println("Test metody subtract:");
+        if(Calculator.subtract(2 ,3 ) == -1){
+            System.out.println("Test zakończony poprawnie!");
+        }
+        else{
+            System.out.println("Test zakończony negatywnie!");
+        }
+
     }
 }
