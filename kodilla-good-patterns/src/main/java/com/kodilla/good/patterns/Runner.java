e9859d05b18f9d9b2bb7f7b85fd609868701754a
SzymonGrzybowicz <szymon.grzybowicz@gmail.com>
2019-06-17 20:37:53 +0200

    Poprawka zadania 13.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Runner.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Runner.java
@@ -2,17 +2,19 @@ package com.kodilla.good.patterns;
 
 import com.kodilla.good.patterns.challenges.MovieStore;
 
-import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Runner {
 
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
 
-        movieStore.getMovies().entrySet().stream()
+        String result = movieStore.getMovies().entrySet().stream()
                 .flatMap(entry -> entry.getValue().stream())
-                .map(name -> name = name + "!")
-                .forEach(System.out::print);
+                .collect(Collectors.joining("!"));
+
+        System.out.println(result);
+
 
 
 
